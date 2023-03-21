3309d5a8ea1bdc8bac1cdeafba8885c6815d1682
Griffin <griffin.wiles1@gmail.com>
2023-03-21 13:29:56 -0400

    Adding the base project to Git

---
+++ .storybook/main.cjs
@@ -0,0 +1,22 @@
+// .storybook/main.js
+
+const { mergeConfig } = require('vite');
+
+module.exports = {
+  stories: ['../src/stories/**/*.stories.mdx', '../src/stories/**/*.stories.@(js|jsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Use the same "resolve" configuration as your app
+      resolve: (await import('../vite.config.js')).default.resolve,
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+      },
+    });
+  },
+};
