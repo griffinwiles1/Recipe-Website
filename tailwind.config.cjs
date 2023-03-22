db1ba890ab8480c901f5f67af2aead47e437be21
Griffin <griffin.wiles1@gmail.com>
2023-03-22 09:28:40 -0400

    Work I've done experimenting with Storybook and connecting it to Vite

---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primaryWhite: "#c4c1bd",
+        secondaryWhite: "#f5f2eb",
+        primaryDark: "#20303b",
+        secondaryDark: "#4a5e67",
+        primaryBlue: "#1997d4",
+        footerPrimary: "#3e525b",
+        footerSecondary: "#93a4ad",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "420px",
+      ss: "620px",
+      sm: "768px",
+      md: "1024px",
+      lg: "1440px",
+      xl: "2000px",
+    },
+  },
+  plugins: [],
+};
